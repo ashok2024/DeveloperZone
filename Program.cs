a2285676ae601cb029c0d09d4f5cf0ab29736357
Ashok Mehta <52393723+ashok2024@users.noreply.github.com>
2022-08-04 11:44:35 +0530

    Add Policy for cross origin

--- Program.cs
+++ Program.cs
@@ -11,6 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<CoreDbContext>(options => 
 options.UseSqlServer("Server=DEVLOPER;initial catalog=VMSDB;Trusted_Connection=True;"));
+
+builder.Services.AddCors(option => {
+    option.AddPolicy("ServiceAPIPolicy", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<IMaster, MasterServices>();
